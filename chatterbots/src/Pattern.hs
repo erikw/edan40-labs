55badb7a4ba08f1a198bbe044288f2d51ebee623
Erik Westrup <erik.westrup@gmail.com>
2013-11-07 22:11:22 +0100

    Substitue and match magically works.

--- chatterbots/src/Pattern.hs
+++ chatterbots/src/Pattern.hs
@@ -1,5 +1,6 @@
 module Pattern where
 import Utilities
+import Data.Maybe(isJust, fromJust)
 
 
 -------------------------------------------------------
@@ -7,24 +8,41 @@ import Utilities
 --------------------------------------------------------
 
 -- Replaces a wildcard in a list with the list given as the third argument
+--                  wildcard -> p -> s
 substitute :: Eq a => a -> [a] -> [a] -> [a]
-substitute _ _ _ = []
-{- TO BE WRITTEN -}
+substitute w [] _ = []
+substitute w (p:ps) s
+                | p == w = s ++ substitute w ps s
+                | otherwise = p : substitute w ps s
 
 
 -- Tries to match two lists. If they match, the result consists of the sublist
 -- bound to the wildcard in the pattern list.
+--              wildcard t s
 match :: Eq a => a -> [a] -> [a] -> Maybe [a]
-match _ _ _ = Nothing
-{- TO BE WRITTEN -}
+{-match w [] [] = Nothing-}
+{-match w [] s = Nothing-}
+match w [] [] = Just []
+match w [] _ = Nothing
+match w p [] = Nothing
+match w pl@(p:ps) sl@(s:ss)
+    | p /= w && p /= s = Nothing
+    | p /= w && p == s = match w ps ss
+    | p == w =  orElse (singleWildcardMatch pl sl) (longerWildcardMatch pl sl)
 
 
 -- Helper function to match
 singleWildcardMatch, longerWildcardMatch :: Eq a => [a] -> [a] -> Maybe [a]
-singleWildcardMatch (wc:ps) (x:xs) = Nothing
-{- TO BE WRITTEN -}
-longerWildcardMatch (wc:ps) (x:xs) = Nothing
-{- TO BE WRITTEN -}
+singleWildcardMatch [] [] = Just []
+{-singleWildcardMatch (wc:ps) (x:xs)-}
+            {-| wc == x = Just (wc :  singleWildcardMatch ps xs)-}
+            {-| otherwise = Nothing-}
+            {-= match (wc:ps) (x:xs)-}
+singleWildcardMatch (wc:ps) (x:xs) = if isJust (match wc ps xs ) then Just [x] else Nothing 
+
+
+{-longerWildcardMatch (wc:ps) (x:xs) =  if isJust (\res -> res = (match wc (wc:ps) xs)) then Just ([x] : (fromJust res)) else Nothing-}
+longerWildcardMatch (wc:ps) (x:xs) = Just (x : fromJust (match wc (wc:ps) xs))
 
 
 
