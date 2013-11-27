3d0480ae76bd44bf4b2f09969f33759e53219ff8
Erik Westrup <erik.westrup@gmail.com>
2013-11-27 23:25:25 +0100

    Begun optAlignments.

--- stringalignment/src/Align.hs
+++ stringalignment/src/Align.hs
@@ -11,7 +11,8 @@ score _ matchSc misSc x y
         | otherwise = misSc
 
 similarityScore :: (Char -> Char -> Int) -> String -> String -> Int
-similarityScore scoreFcn [] y = sum $ map (scoreFcn '-') y
+{-similarityScore scoreFcn [] y = sum $ map (scoreFcn '-') y-}
+similarityScore scoreFcn [] (y:ys) = scoreFcn '-' y  + similarityScore scoreFcn [] ys -- TODO prettier?
 similarityScore scoreFcn x [] = sum $ map (scoreFcn '-') x
 similarityScore scoreFcn (x:xs) (y:ys) = maximum [match, spaceUp, spaceDown]
                                 where
@@ -44,3 +45,29 @@ maximaBy valueFcn xs = map ((!!) xs) maxIndices
 {----Tests----}
 maximaTest = maximaBy length ["cs", "efd", "lth", "it"]
 maximaCheck = maximaTest == ["efd", "lth"]
+
+{----2 d)----}
+type AlignmentType = (String,String)
+
+alignScore :: (Char -> Char -> Int) -> AlignmentType -> Int
+alignScore scorer (str1, str2) = sum $ zipWith scorer str1 str2
+
+optAlignments :: (AlignmentType -> Int) -> String -> String -> [AlignmentType]
+{-optAlignments scoreFcn [] y = [(take (length y) (repeat '-'), y)]-}
+{-optAlignments scoreFcn x [] = [(x, take (length x) (repeat '-'))]-}
+optAlignments scoreFcn [] [] = [("", "")]
+optAlignments scoreFcn [] (y:ys) = attachHeads '-' y (optAlignments scoreFcn [] ys)
+optAlignments scoreFcn (x:xs) [] = attachHeads x '-' (optAlignments scoreFcn xs [])
+optAlignments scoreFcn (x:xs) (y:ys) = maximaBy scoreFcn $ concat [match, spaceUp, spaceDown]
+                                where
+                                match = attachHeads x y (optAlignments scoreFcn xs ys)
+                                spaceUp = attachHeads '-' y (optAlignments scoreFcn (x:xs) ys)
+                                spaceDown = attachHeads x '-' (optAlignments scoreFcn xs (y:ys))
+
+
+{----Tests----}
+alignScoreFcn1 = alignScore score1
+alignTest1 = optAlignments alignScoreFcn1 "writers" "vintner"
+alignExpected1 = [("writ-ers","vintner-"), ("wri-t-ers","-vintner-"), ("wri-t-ers","v-intner-")] 
+alignCheck1 = alignTest1 == alignExpected1
+
