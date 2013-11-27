6a9cda87245a840173bf0a999866510eb44454b1
Erik Westrup <erik.westrup@gmail.com>
2013-11-27 21:33:09 +0100

    similarityScore() done.

--- stringalignment/src/Align.hs
+++ stringalignment/src/Align.hs
@@ -1,18 +1,29 @@
 module Align where
 
-space :: Char
-space = '-'
 
-score (Eq a):: a -> a -> Int
-score space _ = -1
-score _ space = -1
-score x y 
-        | x ==y = 0
-        | otherwise = -1
+{----Similarity scoring----}
+score ::  Int -> Int -> Int -> Char -> Char -> Int
+score spaceSc _ _ '-' _ = spaceSc
+score spaceSc _ _ _ '-' = spaceSc
+score _ matchSc misSc x y
+        | x == y = matchSc
+        | otherwise = misSc
 
-similarityScore :: String -> String -> Int
-similarityScore (x:xs) (y:ys) = maximum [match, spaceUp, spaceDown]
+
+similarityScore :: (Char -> Char -> Int) -> String -> String -> Int
+similarityScore scoreFcn [] y = sum $ map (scoreFcn '-') y
+similarityScore scoreFcn x [] = sum $ map (scoreFcn '-') x
+similarityScore scoreFcn (x:xs) (y:ys) = maximum [match, spaceUp, spaceDown]
                                 where
-                                match = score(x,y) + similarityScore(xs, ys)
-                                spaceUp = score(space,y) + similarityScore(x:xs, ys)
-                                spaceDown = score(x,space) + similarityScore(xs, y:ys)
+                                match = scoreFcn x y + similarityScore scoreFcn xs ys
+                                spaceUp = scoreFcn '-' y  + similarityScore scoreFcn (x:xs) ys
+                                spaceDown = scoreFcn x '-' + similarityScore scoreFcn xs (y:ys)
+
+{----Tests----}
+score1 = score (-1) 0 (-1)
+simTest1 = similarityScore score1 "writers" "vintner"
+simCheck1 = simTest1 == (-5)
+
+score2 = score (-2) 1 (-1)
+simTest2 = similarityScore score2 "HASKELL" "PASCAL"
+simCheck2 = simTest2 == (-2)
