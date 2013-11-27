a3c1f28b26b7fed088db7a9602ca7caf72026f53
Erik Westrup <erik.westrup@gmail.com>
2013-11-27 21:54:12 +0100

    attachHeads() and maximaBy() done.

--- stringalignment/src/Align.hs
+++ stringalignment/src/Align.hs
@@ -1,6 +1,7 @@
 module Align where
+import Data.List(elemIndices)
 
-
+{----2 a)----}
 {----Similarity scoring----}
 score ::  Int -> Int -> Int -> Char -> Char -> Int
 score spaceSc _ _ '-' _ = spaceSc
@@ -8,7 +9,6 @@ score spaceSc _ _ _ '-' = spaceSc
 score _ matchSc misSc x y
         | x == y = matchSc
         | otherwise = misSc
-
 
 similarityScore :: (Char -> Char -> Int) -> String -> String -> Int
 similarityScore scoreFcn [] y = sum $ map (scoreFcn '-') y
@@ -27,3 +27,20 @@ simCheck1 = simTest1 == (-5)
 score2 = score (-2) 1 (-1)
 simTest2 = similarityScore score2 "HASKELL" "PASCAL"
 simCheck2 = simTest2 == (-2)
+
+{----2 b)----}
+-- Prepend h1 and h2 respectivly to each tuple's two lists.
+attachHeads :: a -> a -> [([a],[a])] -> [([a],[a])]
+attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList] 
+
+{----2 c)----}
+maximaBy :: Ord b => (a -> b) -> [a] -> [a]
+maximaBy valueFcn xs = map ((!!) xs) maxIndices
+                    where
+                    xsValues = map valueFcn xs
+                    maxVal = maximum xsValues
+                    maxIndices = elemIndices maxVal xsValues 
+
+{----Tests----}
+maximaTest = maximaBy length ["cs", "efd", "lth", "it"]
+maximaCheck = maximaTest == ["efd", "lth"]
