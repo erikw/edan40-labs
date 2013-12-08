7efbf1665c3638b24f06c01dfdf22cf3bef11255
Erik Westrup <erik.westrup@gmail.com>
2013-12-08 19:06:24 +0100

    Program parsing and execution.

--- functional_parsing/src/Program.hs
+++ functional_parsing/src/Program.hs
@@ -3,9 +3,20 @@ import Parser hiding (T)
 import qualified Statement
 import qualified Dictionary
 import Prelude hiding (return, fail)
-newtype T = Program () -- to be defined
+
+newtype T = Program [Statement.T]
+
+{-iter :: Parser a -> Parser [a]  -}
+{-iter m = m # iter m >-> cons ! return [] -}
+
+{-type Parser a = String -> Maybe (a, String)-}
+-- Parser [Statement.T] ::: String -> Maybe ([Statement.T], String)
+-- Parser Program [Statement.T] ::: String -> Maybe (Program [Statement.T], String)
+
+
 instance Parse T where
-  parse = error "Program.parse not implemented"
+  parse = iter Statement.parse >-> Program
   toString = error "Program.toString not implemented"
              
-exec = error "Program.exec not implemented"
+exec :: T -> [Integer] -> [Integer]
+exec (Program stmts) input = Statement.exec stmts Dictionary.empty input
